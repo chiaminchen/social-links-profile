28be431ccfa2ba3ceb64e58cc677d4a37872c866
ChiaMin <chen.chiamin@gmail.com>
2026-04-11 08:27:20 +0800

    feat: initialize social links profile project with React, TypeScript, and Tailwind CSS
    - Add package.json with dependencies and scripts for development and build
    - Include favicon and screenshots for the project
    - Create main application component with links to social profiles
    - Add CSS for styling using Tailwind and custom variables
    - Set up TypeScript configuration for app and node
    - Configure Vite for development and build process

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import jessicaImage from './assets/avatar-jessica.jpeg';
+
+interface LinksType {
+  website: string;
+  url: string;
+}
+
+const links: LinksType[] = [
+  { website: 'GitHub', url: 'https://github.com/' },
+  { website: 'Frontend Mentor', url: 'https://www.frontendmentor.io/' },
+  { website: 'LinkedIn', url: 'https://www.linkedin.com/' },
+  { website: 'Twitter', url: 'https://www.twitter.com/' },
+  { website: 'Instagram', url: 'https://www.instagram.com/' },
+];
+
+function App() {
+  return (
+    <main className="bg-gray-medium text-white w-card-fluid rounded-xl p-6 flex flex-col gap-6 items-center text-center">
+      <img className="w-22 rounded-[50%]" src={jessicaImage} alt="Jessica" />
+      <section>
+        <h1 className="text-2xl/normal font-bold">Jessica Randall</h1>
+        <p className="text-green text-sm/normal font-bold">London, United Kingdom</p>
+      </section>
+      <p className="text-sm/normal">"Front-end developer and avid reader."</p>
+      <ul className="flex flex-col gap-4 w-full">
+        {links.map(link => (
+          <Link website={link.website} url={link.url} />
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+function Link({ website, url }: LinksType) {
+  return (
+    <li>
+      <a
+        className="bg-gray-light hover:bg-green p-3 rounded-lg text-sm/normal font-bold  hover:text-gray-medium block transition-all duration-300"
+        href={url}
+        target="_blank"
+      >
+        {website}
+      </a>
+    </li>
+  );
+}
+
+export default App;
