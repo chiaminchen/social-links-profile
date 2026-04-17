88e8102a7831c5ae14d948bc1498a7e60c9d506a
ChiaMin <chen.chiamin@gmail.com>
2026-04-17 22:43:46 +0800

    refactor: restructure App component layout for improved readability

--- src/App.tsx
+++ src/App.tsx
@@ -15,30 +15,32 @@ const links: SocialLinkType[] = [
 
 function App() {
   return (
-    <main className="bg-gray-medium w-card-fluid flex flex-col items-center gap-6 rounded-xl p-6 text-center text-white">
-      <img
-        className="w-22 rounded-full"
-        src={jessicaImage}
-        alt="Jessica Randall"
-        width={88}
-        height={88}
-      />
-      <div>
-        <h1 className="text-2xl/normal font-bold">Jessica Randall</h1>
-        <p className="text-green text-sm/normal font-bold">
-          London, United Kingdom
-        </p>
-      </div>
-      <p className="text-sm/normal">"Front-end developer and avid reader."</p>
-      <ul className="flex w-full flex-col gap-4">
-        {links.map((link) => (
-          <SocialLink
-            key={link.website}
-            website={link.website}
-            url={link.url}
-          />
-        ))}
-      </ul>
+    <main>
+      <article className="font-inter bg-gray-medium w-card-fluid flex flex-col items-center gap-6 rounded-xl p-6 text-center text-white">
+        <img
+          className="w-22 rounded-full"
+          src={jessicaImage}
+          alt="Jessica Randall"
+          width={88}
+          height={88}
+        />
+        <div>
+          <h1 className="text-2xl/normal font-bold">Jessica Randall</h1>
+          <p className="text-green text-sm/normal font-bold">
+            London, United Kingdom
+          </p>
+        </div>
+        <p className="text-sm/normal">"Front-end developer and avid reader."</p>
+        <ul className="flex w-full flex-col gap-4">
+          {links.map((link) => (
+            <SocialLink
+              key={link.website}
+              website={link.website}
+              url={link.url}
+            />
+          ))}
+        </ul>
+      </article>
     </main>
   );
 }
